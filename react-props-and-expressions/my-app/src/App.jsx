15ca5c83c97b17f21dcc4fb83c6962e4573c93d1
Vorleak Yek <vorleakyek@gmail.com>
2023-09-01 00:04:28 +0000

    Change props values in the App component.

--- react-props-and-expressions/my-app/src/App.jsx
+++ react-props-and-expressions/my-app/src/App.jsx
@@ -4,9 +4,9 @@ import CustomButton from './CustomButton';
 function App() {
   return (
     <div>
-      <CustomButton color={'red'} text={'I'} />
-      <CustomButton color={'green'} text={'know'} />
-      <CustomButton color={'blue'} text={'React!'} />
+      <CustomButton color="red" text="I" />
+      <CustomButton color="green" text="know" />
+      <CustomButton color="blue" text="React!" />
     </div>
   );
 }
