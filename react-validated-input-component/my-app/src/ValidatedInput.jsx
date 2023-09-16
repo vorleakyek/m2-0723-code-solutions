720130deadbd36a07a294b56616b331d64f00737
Vorleak Yek <vorleakyek@gmail.com>
2023-09-16 04:25:49 +0000

    Update to only use one state with an object as an initial value.

--- react-validated-input-component/my-app/src/ValidatedInput.jsx
+++ react-validated-input-component/my-app/src/ValidatedInput.jsx
@@ -3,42 +3,47 @@ import { FaX } from 'react-icons/fa6';
 import { FaCheck } from 'react-icons/fa6';
 
 export default function ValidatedInput() {
-  const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [iconWrapper, setIconWrapper] = useState('icon-wrapper hidden');
-  const [icon, setIcon] = useState('');
-
-  function updateSetStates(errorMessage, iconWrapper, reactIcon) {
-    setErrorMessage(errorMessage);
-    setIconWrapper(iconWrapper);
-    setIcon(reactIcon);
-  }
+  const [data, setData] = useState({
+    password: '',
+    iconWrapper: '',
+    icon: '',
+    errorMessage: '',
+  });
 
   function handleValidation(e) {
     const { value } = e.target;
     const regEx = /^(?=.*\d)(?=.*[A-Z])(?=.*\W).+/;
     const passRegExTest = regEx.test(value);
-    setPassword(value);
+
     if (value.length === 0) {
-      updateSetStates(
-        'A password is required.',
-        'icon-wrapper',
-        <FaX className="icon" />
-      );
+      setData({
+        password: value,
+        errorMessage: 'A password is required.',
+        iconWrapper: 'icon-wrapper',
+        icon: <FaX className="icon" />,
+      });
     } else if (value.length > 0 && value.length < 8) {
-      updateSetStates(
-        'Your password is too short.',
-        'icon-wrapper',
-        <FaX className="icon" />
-      );
+      setData({
+        password: value,
+        errorMessage: 'Your password is too short.',
+        iconWrapper: 'icon-wrapper',
+        icon: <FaX className="icon" />,
+      });
     } else if (value.length >= 8 && !passRegExTest) {
-      updateSetStates(
-        'Your password should contain a digit, a capital letter, and a special character.',
-        'icon-wrapper',
-        <FaX className="icon" />
-      );
+      setData({
+        password: value,
+        errorMessage:
+          'Your password should contain a digit, a capital letter, and a special character.',
+        iconWrapper: 'icon-wrapper',
+        icon: <FaX className="icon" />,
+      });
     } else if (value.length >= 8 && passRegExTest) {
-      updateSetStates('', 'icon-wrapper', <FaCheck className="check" />);
+      setData({
+        password: value,
+        errorMessage: '',
+        iconWrapper: 'icon-wrapper',
+        icon: <FaCheck className="check" />,
+      });
     }
   }
 
@@ -51,14 +56,14 @@ export default function ValidatedInput() {
           id="password"
           type="password"
           className="form-control"
-          value={password}
+          value={data.password}
           onChange={handleValidation}
           required
         />
-        <div className={iconWrapper}>{icon}</div>
+        <div className={data.iconWrapper}>{data.icon}</div>
       </div>
       <div>
-        <p className="message">{errorMessage}</p>
+        <p className="message">{data.errorMessage}</p>
       </div>
     </div>
   );
